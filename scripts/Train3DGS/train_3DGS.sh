cf8431d91a3fcdfe5ccf654b7c55e95353f8b5ae
Devin100086 <1316048305@qq.com>
2025-03-23 10:51:49 +0800

    update

--- scripts/Train3DGS/train_3DGS.sh
+++ scripts/Train3DGS/train_3DGS.sh
@@ -1,1 +1,9 @@
-python train.py -s /home/wucunqi/Desktop/datasets/face
+#!/bin/bash
+
+DATASET_Path="/home/wucunqi/Desktop/datasets/face"
+
+if [ ! -z "$1" ]; then
+    DATASET_Path="$1"
+fi
+
+python trainer/origin/train.py -s ${DATASET_Path}
